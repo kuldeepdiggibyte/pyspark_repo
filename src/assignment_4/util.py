fa51887dd8696d0ecca89da0fc06c689b1fcabf8
kuldeepdiggibyte <kp.managuli2000@gmail.com>
2024-04-08 01:41:40 +0530

    updated util file for assignment 4

--- src/assignment_4/util.py
+++ src/assignment_4/util.py
@@ -1,83 +1,65 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import explode, posexplode, explode_outer, posexplode_outer, current_date, year, month, day
+from pyspark.sql.functions import explode, current_date, year, month, day, posexplode, explode_outer, posexplode_outer
+from pyspark.sql.types import StringType, StructType, IntegerType, StructField, ArrayType
 
-spark = SparkSession
+def create_session():
+    spark=SparkSession.builder.master("local[*]").appName("Krishna").getOrCreate()
+    return spark
 
-def read_json_file(spark, file_path, schema):
 
-    json_data_frame = spark.read.json(file_path, multiLine=True, schema=schema)
-    return json_data_frame
+path="../../resource/nested_json_file.json"
 
-def flatten_dataframe(df):
+schema=StructType([
+    StructField("id",IntegerType(),True),
+    StructField("properties",StructType([
+        StructField("name",StringType(),True),
+        StructField("storeSize",StringType(),True)
+    ]),True),
+    StructField("employees",ArrayType(StructType([
+        StructField("empId",IntegerType(),True),
+        StructField("empName",StringType(),True)
+    ]),True),True)
+])
+def read_file(spark,path,schema):
+    df=spark.read.json(path,schema=schema,multiLine=True)
+    return df
 
-    flatten_df = df.select("*", "properties.name", "properties.storeSize").drop("properties") \
-        .select("*", explode("employees").alias("new_employees")).drop("employees") \
-        .select("*", "new_employees.empId", "new_employees.empName").drop("new_employees")
-    return flatten_df
+def flat_df(df):
+    temp_flat_df = df.withColumn("employee", explode("employees")).drop("employees")
 
-def filter_dataframe(df, condition):
+    flatted_df = temp_flat_df.withColumn("empId", temp_flat_df.employee.empId).withColumn("empName",
+                                                                                          temp_flat_df.employee.empName) \
+        .withColumn("name", temp_flat_df.properties['name']).withColumn("storeSize",
+                                                                        temp_flat_df.properties['storeSize']).drop(
+        "employee", "properties")
+    return flatted_df
 
-    filtered_df = df.filter(condition)
-    return filtered_df
+def explode_display(read_df):
+    return read_df.select(read_df.id, read_df.properties, explode(read_df.employees))
+def explode_outer_display(read_df):
+    return read_df.select(read_df.id, read_df.properties, explode_outer(read_df.employees))
+def posexplode_display(read_df):
+    return read_df.select(read_df.id, read_df.properties, posexplode(read_df.employees))
+def posexplode_outer_display(read_df):
+    return read_df.select(read_df.id, read_df.properties, posexplode_outer(read_df.employees))
 
-def convert_to_snake_case(data_frame):
+def check_id(df):
+    return df.filter(df.id == "0001")
 
-    for column in data_frame.columns:
-        snake_case_col = ''.join(['_' + char.lower() if char.isupper() else char for char in column]).lstrip('_')
-        data_frame = data_frame.withColumnRenamed(column, snake_case_col)
-    return data_frame
+def convert_lower(txt):
+    new=""
+    for i in txt:
+        if i.islower():
+            new=new+"".join(i)
+        else:
+            temp=f"_{i}"
+            new=new+"".join(temp.lower())
+    return new
 
-def add_load_date_column(df):
-
-    load_date_df = df.withColumn("load_date", current_date())
-    return load_date_df
-
-def add_date_columns(df, date_column="load_date"):
-
-    date_df = df.withColumn("year", year(df[date_column])) \
-                .withColumn("month", month(df[date_column])) \
-                .withColumn("day", day(df[date_column]))
+def add_current_date(df):
+    date_df = df.withColumn("load_date", current_date())
     return date_df
 
-if __name__ == "__main__":
-    # Create Spark session
-    spark = create_spark_session()
-
-    # Define schema for JSON data
-    json_schema = StructType([
-        StructField("id", IntegerType(), True),
-        StructField("properties", StructType([
-            StructField("name", StringType(), True),
-            StructField("storeSize", StringType(), True)
-        ]), True),
-        StructField("employees", ArrayType(
-            StructType([
-                StructField("empId", LongType(), True),
-                StructField("empName", StringType(), True)
-            ])
-        ), True)
-    ])
-
-    # Path to JSON file
-    json_file_path = "../resource/nested_json_file.json"
-
-    # Read JSON file
-    json_data_frame = read_json_file(spark, json_file_path, json_schema)
-
-    # 1. Flatten the DataFrame
-    flatten_df = flatten_dataframe(json_data_frame)
-
-    # 2. Filter DataFrame
-    filtered_df = filter_dataframe(flatten_df, flatten_df["empId"] == 1001)
-
-    # 3. Convert column names to snake_case
-    snake_case_df = convert_to_snake_case(flatten_df)
-
-    # 4. Add a new column named 'load_date' with the current date
-    load_date_df = add_load_date_column(snake_case_df)
-
-    # 5. Add separate year, month, and day columns from the 'load_date' column
-    year_month_day_df = add_date_columns(load_date_df)
-
-    # Show DataFrame
-    year_month_day_df.show()
+def add_year_month(date_df):
+    result = date_df.withColumn("year", year(date_df["load_date"])).withColumn("month", month(date_df["load_date"])).withColumn("day", day(date_df["load_date"]))
+    return result
